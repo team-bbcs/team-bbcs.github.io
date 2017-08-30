b7ed3d3225ce56f05aa7d36ab8bc24812c4cdb4d
Suhail Singh Bains <suhail.singhyo@gmail.com>
2017-08-31 01:13:03 +0530

    Added a Front-Page
    Front-Page is animated

--- js/script.js
+++ js/script.js
@@ -1,48 +1,116 @@
 jQuery(document).ready(function($){
-  //move nav element position according to window width
-  moveNavigation();
-  $(window).on('resize', function(){
-    (!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
-  });
+	//cache some jQuery objects
+	var modalTrigger = $('.cd-modal-trigger'),
+		transitionLayer = $('.cd-transition-layer'),
+		transitionBackground = transitionLayer.children(),
+		modalWindow = $('.cd-modal');
+    var link = document.getElementById("front_page_button");
+    document.onkeydown = function (e) {
+      if (e.keyCode == 32) {
+          link.click();
+      }
+  };
 
-  //mobile version - open/close navigation
-  $('.cd-nav-trigger').on('click', function(event){
+	var frameProportion = 1.78, //png frame aspect ratio
+		frames = 25, //number of png frames
+		resize = false;
+
+	//set transitionBackground dimentions
+	setLayerDimensions();
+	$(window).on('resize', function(){
+		if( !resize ) {
+			resize = true;
+			(!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300) : window.requestAnimationFrame(setLayerDimensions);
+		}
+	});
+
+	//open modal window
+  modalTrigger.on('click', function(event){
+    var url = event.target.href || event.srcElement.href;
     event.preventDefault();
-    if($('header').hasClass('nav-is-visible')) $('.moves-out').removeClass('moves-out');
+    transitionLayer.addClass('visible opening');
+    var delay = ( $('.no-cssanimations').length > 0 ) ? 0 : 600;
+    setTimeout(function(){
+      window.open(url, "_self");
+    }, delay);
+});
 
-    $('header').toggleClass('nav-is-visible');
-    $('.cd-main-nav').toggleClass('nav-is-visible');
-    $('.cd-main-content').toggleClass('nav-is-visible');
-  });
+	//close modal window
+	modalWindow.on('click', '.modal-close', function(event){
+		event.preventDefault();
+		transitionLayer.addClass('closing');
+		modalWindow.removeClass('visible');
+		transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+			transitionLayer.removeClass('closing opening visible');
+			transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
+		});
+	});
 
-  //mobile version - go back to main navigation
-  $('.go-back').on('click', function(event){
-    event.preventDefault();
-    $('.cd-main-nav').removeClass('moves-out');
-  });
+	function setLayerDimensions() {
+		var windowWidth = $(window).width(),
+			windowHeight = $(window).height(),
+			layerHeight, layerWidth;
 
-  //open sub-navigation
-  $('.cd-subnav-trigger').on('click', function(event){
-    event.preventDefault();
-    $('.cd-main-nav').toggleClass('moves-out');
-  });
+		if( windowWidth/windowHeight > frameProportion ) {
+			layerWidth = windowWidth;
+			layerHeight = layerWidth/frameProportion;
+		} else {
+			layerHeight = windowHeight*1.2;
+			layerWidth = layerHeight*frameProportion;
+		}
 
-  function moveNavigation(){
-    var navigation = $('.cd-main-nav-wrapper');
-      var screenSize = checkWindowWidth();
-        if ( screenSize ) {
-          //desktop screen - insert navigation inside header element
-      navigation.detach();
-      navigation.insertBefore('.cd-nav-trigger');
-    } else {
-      //mobile screen - insert navigation after .cd-main-content element
-      navigation.detach();
-      navigation.insertAfter('.cd-main-content');
+		transitionBackground.css({
+			'width': layerWidth*frames+'px',
+			'height': layerHeight+'px',
+		});
+
+		resize = false;
+	}
+
+    //move nav element position according to window width
+    moveNavigation();
+    $(window).on('resize', function(){
+      (!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
+    });
+
+    //mobile version - open/close navigation
+    $('.cd-nav-trigger').on('click', function(event){
+      event.preventDefault();
+      if($('header').hasClass('nav-is-visible')) $('.moves-out').removeClass('moves-out');
+
+      $('header').toggleClass('nav-is-visible');
+      $('.cd-main-nav').toggleClass('nav-is-visible');
+      $('.cd-main-content').toggleClass('nav-is-visible');
+    });
+
+    //mobile version - go back to main navigation
+    $('.go-back').on('click', function(event){
+      event.preventDefault();
+      $('.cd-main-nav').removeClass('moves-out');
+    });
+
+    //open sub-navigation
+    $('.cd-subnav-trigger').on('click', function(event){
+      event.preventDefault();
+      $('.cd-main-nav').toggleClass('moves-out');
+    });
+
+    function moveNavigation(){
+      var navigation = $('.cd-main-nav-wrapper');
+        var screenSize = checkWindowWidth();
+          if ( screenSize ) {
+            //desktop screen - insert navigation inside header element
+        navigation.detach();
+        navigation.insertBefore('.cd-nav-trigger');
+      } else {
+        //mobile screen - insert navigation after .cd-main-content element
+        navigation.detach();
+        navigation.insertAfter('.cd-main-content');
+      }
     }
-  }
 
-  function checkWindowWidth() {
-    var mq = window.getComputedStyle(document.querySelector('header'), '::before').getPropertyValue('content').replace(/"/g, '').replace(/'/g, "");
-    return ( mq == 'mobile' ) ? false : true;
-  }
+    function checkWindowWidth() {
+      var mq = window.getComputedStyle(document.querySelector('header'), '::before').getPropertyValue('content').replace(/"/g, '').replace(/'/g, "");
+      return ( mq == 'mobile' ) ? false : true;
+    }
 });

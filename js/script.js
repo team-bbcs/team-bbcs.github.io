a3cd6134a2c0f798771d9961b0b72252c8b3a47c
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2017-10-26 15:55:14 +0530

    commiting to revert
    dont know how to revert, so using a commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,5 @@
 jQuery(document).ready(function($){
-//move butterfly
-var container = document.getElementById('container1');
-var windowHeight = window.innerHeight;
-var windowWidth = window.innerWidth;
-var scrollArea = 1000 - windowHeight;
-var square1 = document.getElementsByClassName('bee')[0];
-
-
-// update position of square 1 and square 2 when scroll event fires.
-window.addEventListener('scroll', function() {
-  var scrollTop = window.pageYOffset || window.scrollTop;
-  var scrollPercent = scrollTop/scrollArea || 0;
-  
-  square1.style.left = scrollPercent*window.innerWidth + 'px';
-  
-});
-
-
-	//parallax
-$('.parallax').parallax();
-  //cache some jQuery objects
-  
-
+	//cache some jQuery objects
 	var modalTrigger = $('.cd-modal-trigger'),
 		transitionLayer = $('.cd-transition-layer'),
 		transitionBackground = transitionLayer.children(),
